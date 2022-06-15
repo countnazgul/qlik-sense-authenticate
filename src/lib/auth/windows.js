56575034b367dad23734457393253a102c6fe3e4
Stefan Stoichev <stefan.stoichev@gmail.com>
2022-06-15 10:07:12 +0100

    fix - return the real msg if an error when checking for active session

--- src/lib/auth/windows.js
+++ src/lib/auth/windows.js
@@ -9,25 +9,21 @@ const win = async function (config) {
   //  * make request to the login url, providing the user/pass in the body
   //  * if ok - extract the session Id from the set-session header
 
-  let initialChecksSession = await initialChecks.combined(config);
+  const initialChecksSession = await initialChecks.combined(config);
 
   if (initialChecksSession.error) {
     // if there is no session file
     // or the saved session is no longer valid
     // start the process to get new session
-    let loginLocation = await firstRequest(config);
+    const loginLocation = await firstRequest(config);
 
-    if (loginLocation.error) {
-      return loginLocation;
-    }
+    if (loginLocation.error) return loginLocation;
 
     config.loginLocation = loginLocation.message.headers.location;
 
-    let sessionId = await secondRequest(config);
+    const sessionId = await secondRequest(config);
 
-    if (sessionId.error) {
-      return sessionId;
-    }
+    if (sessionId.error) return sessionId;
 
     helpers.session.write(sessionId.message);
 
@@ -42,29 +38,25 @@ const initialChecks = {
     return helpers.session.read();
   },
   sessionIsActive: async function (config, sessionId) {
-    let checkSessionRequest = await helpers.webRequest.get({
+    const checkSessionRequest = await helpers.webRequest.get({
       url: `${config.url.replace("/hub/", "")}/qps/user`,
       headers: { Cookie: `${config.header}=${sessionId}` },
     });
 
-    // if (!checkSessionRequest.data.session) {
-    //     return { error: true, message: 'session is expired' }
-    // }
+    if (checkSessionRequest.error == true) return checkSessionRequest;
 
-    if (checkSessionRequest.message.data.session == "inactive") {
+    if (checkSessionRequest.message.data.session == "inactive")
       return { error: true, message: "session is expired" };
-    }
 
     return { error: false, message: sessionId };
   },
   combined: async function (config) {
-    let savedSession = initialChecks.savedSession();
+    const savedSession = initialChecks.savedSession();
 
-    if (savedSession.error == true) {
+    if (savedSession.error == true)
       return { error: true, message: savedSession.message };
-    }
 
-    let isSessionActive = await this.sessionIsActive(
+    const isSessionActive = await this.sessionIsActive(
       config,
       savedSession.message
     );
@@ -82,7 +74,7 @@ async function firstRequest(config) {
   // rather that its a Form authentication
   // "converting" from Windows to Form is done via the User-Agent header
   // Trying to make proper Windows request from Node is a bit of a pain ... for now
-  let response = await helpers.webRequest.get({
+  const response = await helpers.webRequest.get({
     url: config.url,
     headers: { "User-Agent": "Form" },
   });
@@ -92,7 +84,7 @@ async function firstRequest(config) {
 
 async function secondRequest(config) {
   // split the url params to get the correct xrfkey
-  let urlParams = url.parse(config.loginLocation, {
+  const urlParams = url.parse(config.loginLocation, {
     parseQueryString: true,
   }).query;
 
@@ -100,11 +92,9 @@ async function secondRequest(config) {
   //  * content type should be urlencoded
   //  * use the correct xrfkey
 
-  if (!urlParams.xrfkey) {
-    urlParams.xrfkey = helpers.generateXrfkey(16);
-  }
+  if (!urlParams.xrfkey) urlParams.xrfkey = helpers.generateXrfkey(16);
 
-  let reqOptions = {
+  const reqOptions = {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
       "x-qlik-xrfkey": urlParams.xrfkey,
@@ -112,40 +102,33 @@ async function secondRequest(config) {
   };
 
   // convert the body (user and pass) to url encoded values
-  let queryCredentials = helpers.convertToFormData({
+  const queryCredentials = helpers.convertToFormData({
     username: config.username,
     password: config.password,
   });
 
   // make the request
-  let response = await helpers.webRequest.post({
+  const response = await helpers.webRequest.post({
     url: `${config.loginLocation}`,
     headers: reqOptions,
     body: queryCredentials,
   });
 
-  if (response.error) {
-    return response.error;
-  }
+  if (response.error) return response.error;
 
   // if there is a response but there is no cookie
   // the the authentication was not successful - wrong user/pass
-  if (!response.message.headers["set-cookie"]) {
+  if (!response.message.headers["set-cookie"])
     return {
       error: true,
       message: "Authentication failed. Wrong username and/or password?",
     };
-  }
 
   // extract the session id from the response headers
-  let sessionId = helpers.extractSessionId({
+  const sessionId = helpers.extractSessionId({
     headers: response.message.headers,
     config,
   });
-
-  if (sessionId.error) {
-    return sessionId;
-  }
 
   return sessionId;
 }
